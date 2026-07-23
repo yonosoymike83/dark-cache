39d4ccb1eb8a5b5975dbdb6fc372706901b99cf7
Mike <mike.knoppix@gmail.com>
2026-07-23 23:25:37 +0200

    Update play.js

--- play.js
+++ play.js
@@ -1,3 +1,73 @@
+const PASSWORD = "negro_oscuro";
+
+const saved = localStorage.getItem("darkcache_password");
+
+const passwordScreen = document.getElementById("passwordScreen");
+const input = document.getElementById("passwordInput");
+const button = document.getElementById("passwordButton");
+const error = document.getElementById("passwordError");
+
+function startGame(){
+
+    passwordScreen.style.display="none";
+
+    startIntro(
+
+        introTexts,
+
+        "CAPÍTULO I",
+
+        "El viejo tendero",
+
+        ()=>{
+
+            document.getElementById("intro").innerHTML="ESCENA 1";
+            document.getElementById("intro").style.opacity=1;
+
+        }
+
+    );
+
+}
+
+if(saved===PASSWORD){
+
+    startGame();
+
+}else{
+
+    input.focus();
+
+    function checkPassword(){
+
+        if(input.value===PASSWORD){
+
+            localStorage.setItem("darkcache_password",PASSWORD);
+
+            startGame();
+
+        }else{
+
+            error.textContent="Contraseña incorrecta";
+
+            input.value="";
+        }
+
+    }
+
+    button.onclick=checkPassword;
+
+    input.addEventListener("keydown",e=>{
+
+        if(e.key==="Enter"){
+
+            checkPassword();
+
+        }
+
+    });
+
+}
 const introTexts=[
 
 "Dicen que algunos geocachés son imposibles de encontrar.",
