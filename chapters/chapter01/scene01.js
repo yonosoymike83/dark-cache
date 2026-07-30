8e0acc621471b5cc09919fa1016d9403520ceb7e
Mike <mike.knoppix@gmail.com>
2026-07-30 13:54:38 +0200

    Update scene01.js

--- chapters/chapter01/scene01.js
+++ chapters/chapter01/scene01.js
@@ -2,9 +2,13 @@ const Scene01 = {
 
     id: "scene01",
 
+    name: "La Tienda del Viejo Bug",
+
     image: "assets/images/escena01.png",
 
     music: "assets/music/escena01.mp3",
+
+    ambient: "assets/sounds/rain.ogg",
 
     hotspots: [
 
