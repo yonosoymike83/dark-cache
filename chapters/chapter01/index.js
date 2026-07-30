afb6a18f891e43c248839c76ebc5587c0e98bebe
Mike <mike.knoppix@gmail.com>
2026-07-30 08:20:04 +0200

    Create index.js

---
+++ chapters/chapter01/index.js
@@ -0,0 +1,35 @@
+const Chapter01 = {
+
+    id: "chapter01",
+
+    title: "CAPÍTULO I",
+
+    subtitle: "La Tienda del Viejo Bug",
+
+    intro: [
+
+        "Dicen que algunos cachés son imposibles de encontrar.",
+
+        "Otros...",
+
+        "que nunca debieron haberse ocultado.",
+
+        "Durante años pensé que solo eran historias.",
+
+        "Hasta aquella noche.",
+
+        "Alguien pronunció un nombre en voz baja...",
+
+        "El Caché Oscuro.",
+
+        "Y se hizo el silencio, nadie quiso seguir hablando...",
+
+        "Pero yo insistí, aunque solo para oír:",
+
+        "La Tienda del Viejo Bug."
+
+    ],
+
+    firstScene: Scene01
+
+};
