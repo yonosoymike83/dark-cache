3fc99435d05499622d17d82880fcefbe672e15ef
Mike <mike.knoppix@gmail.com>
2026-08-03 00:24:47 +0200

    Create dialogs.js

---
+++ chapters/chapter01/dialogs.js
@@ -0,0 +1,67 @@
+// ======================================================
+// CAPÍTULO I
+// LA TIENDA DEL VIEJO BUG
+// DIÁLOGOS
+// ======================================================
+
+
+// ------------------------------------------------------
+// SCENE 01
+// EXTERIOR DE LA TIENDA
+// ------------------------------------------------------
+
+const DialogEnterShop = [
+
+    {
+
+        text: "La puerta está entreabierta."
+
+    },
+
+    {
+
+        text: "Quizá aquí encuentre alguna respuesta."
+
+    },
+
+    {
+
+        text: "Será mejor entrar."
+
+    }
+
+];
+
+
+// ------------------------------------------------------
+// EJEMPLO DE CONVERSACIÓN
+// (Todavía no se usa)
+// ------------------------------------------------------
+
+const DialogBugWelcome = [
+
+    {
+
+        speaker: "Viejo Bug",
+
+        text: "Buenas noches..."
+
+    },
+
+    {
+
+        speaker: "Jugador",
+
+        text: "Busco el Caché Oscuro."
+
+    },
+
+    {
+
+        speaker: "Viejo Bug",
+
+        text: "Entonces has venido al lugar adecuado..."
+
+    }
+
+];
