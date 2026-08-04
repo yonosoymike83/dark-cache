730d47b20347c6fe32ee50fe80ced4b30af9f309
Mike <mike.knoppix@gmail.com>
2026-08-04 21:53:46 +0200

    Update hotspot-editor.js

--- dc-engine/hotspot-editor.js
+++ dc-engine/hotspot-editor.js
@@ -69,15 +69,70 @@ class HotspotEditor {
 
     }
 
-    startDraw(e){
+    startDraw(e) {
+
+        this.dragging = true;
+
+        const rect = document
+            .getElementById("scene")
+            .getBoundingClientRect();
+
+        this.startX = e.clientX - rect.left;
+        this.startY = e.clientY - rect.top;
+
+        this.preview = document.createElement("div");
+
+        this.preview.style.position = "absolute";
+
+        this.preview.style.left = this.startX + "px";
+        this.preview.style.top = this.startY + "px";
+
+        this.preview.style.width = "0px";
+        this.preview.style.height = "0px";
+
+        this.preview.style.background = "rgba(0,255,120,.20)";
+        this.preview.style.border = "2px solid #00ff66";
+
+        this.preview.style.pointerEvents = "none";
+
+        document
+            .getElementById("scene")
+            .appendChild(this.preview);
 
     }
 
-    updateDraw(e){
+    updateDraw(e) {
+
+        if (!this.dragging) return;
+
+        const rect = document
+            .getElementById("scene")
+            .getBoundingClientRect();
+
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const left = Math.min(this.startX, x);
+        const top = Math.min(this.startY, y);
+
+        const width = Math.abs(x - this.startX);
+        const height = Math.abs(y - this.startY);
+
+        this.preview.style.left = left + "px";
+        this.preview.style.top = top + "px";
+
+        this.preview.style.width = width + "px";
+        this.preview.style.height = height + "px";
 
     }
 
-    finishDraw(e){
+    finishDraw(e) {
+
+        if (!this.dragging) return;
+
+        this.dragging = false;
+
+        console.log("Hotspot creado.");
 
     }
 
