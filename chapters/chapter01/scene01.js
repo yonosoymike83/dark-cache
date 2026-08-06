fd36bb8ae4f175be8162d251600b11ff52c8c7e8
Mike <mike.knoppix@gmail.com>
2026-08-06 16:01:23 +0200

    Update scene01.js

--- chapters/chapter01/scene01.js
+++ chapters/chapter01/scene01.js
@@ -1,3 +1,8 @@
+// ======================================================
+// ESCENA 01
+// EXTERIOR DE LA TIENDA
+// ======================================================
+
 const Scene01 = {
 
     id: "scene01",
@@ -10,135 +15,177 @@ const Scene01 = {
 
     ambient: "assets/sounds/rain.ogg",
 
+    // Hotspots obligatorios para poder entrar
+
+    requiredHotspots: [
+
+        "cartel01",
+        "cartel02",
+        "placa",
+        "mostrador",
+        "cat"
+
+    ],
+
+    visitedHotspots: [],
+
     hotspots: [
-                {
-        
+
+        {
             id: "campanario",
-        
+
             x: 21.65,
             y: 15.78,
             width: 5.20,
             height: 17.96,
-        
+
             click(){
-        
+
+                dialog.show(DialogCampanario);
+
             }
-        
+
         },
-                {
-        
+
+        {
             id: "light",
-        
+
             x: 9.15,
             y: 5.58,
             width: 6.94,
             height: 16.15,
-        
+
             click(){
-        
+
+                dialog.show(DialogLight);
+
             }
-        
+
         },
-                {
-        
+
+        {
             id: "cartel01",
-        
+
             x: 59.15,
             y: 17.16,
             width: 10.11,
             height: 10.84,
-        
+
             click(){
-        
+
+                visitHotspot("cartel01");
+
+                dialog.show(DialogCartel01);
+
             }
-        
+
         },
-                {
-        
+
+        {
             id: "cartel02",
-        
+
             x: 72.79,
             y: 12.06,
             width: 6.28,
             height: 12.75,
-        
+
             click(){
-        
+
+                visitHotspot("cartel02");
+
+                dialog.show(DialogCartel02);
+
             }
-        
+
         },
-                {
-        
+
+        {
             id: "placa",
-        
+
             x: 89.06,
             y: 18.97,
             width: 6.82,
             height: 11.05,
-        
+
             click(){
-        
+
+                visitHotspot("placa");
+
+                dialog.show(DialogPlaca);
+
             }
-        
+
         },
-                {
-        
+
+        {
             id: "mostrador",
-        
+
             x: 67.17,
             y: 36.72,
             width: 13.28,
             height: 25.40,
-        
+
             click(){
-        
+
+                visitHotspot("mostrador");
+
+                dialog.show(DialogMostrador);
+
             }
-        
+
         },
-                {
-        
+
+        {
             id: "alcantarilla",
-        
+
             x: 29.07,
             y: 94.00,
             width: 11.24,
             height: 5.53,
-        
+
             click(){
-        
+
+                dialog.show(DialogAlcantarilla);
+
             }
-        
+
         },
-                {
-        
+
+        {
             id: "trash",
-        
+
             x: 84.75,
             y: 69.98,
             width: 7.12,
             height: 16.79,
-        
+
             click(){
-        
+
+                dialog.show(DialogTrash);
+
             }
-        
+
         },
+
         {
-        
             id: "cat",
-        
+
             x: 76.32,
             y: 81.46,
             width: 6.04,
             height: 15.52,
-        
-            click(){
-        
-            }
-        
-        },
-        {
 
+            click(){
+
+                visitHotspot("cat");
+
+                dialog.show(DialogCat);
+
+            }
+
+        },
+
+        {
             id: "door",
 
             x: 59.02,
@@ -146,22 +193,27 @@ const Scene01 = {
             width: 3.49,
             height: 4.75,
 
-            click() {
+            click(){
 
-                dialog.show(
+                if(canEnterShop()){
 
-                    DialogEnterShop,
+                    dialog.show(
 
-                    function(){
+                        DialogEnterShop,
 
-                        // Más adelante:
-                        // sceneManager.load(Scene02);
+                        function(){
 
-                        console.log("Entrar en la tienda");
+                            sceneManager.load(Scene02);
 
-                    }
+                        }
 
-                );
+                    );
+
+                }else{
+
+                    dialog.show(DialogDoorLocked);
+
+                }
 
             }
 
@@ -170,3 +222,28 @@ const Scene01 = {
     ]
 
 };
+
+
+// ======================================================
+// FUNCIONES AUXILIARES
+// ======================================================
+
+function visitHotspot(id){
+
+    if(!Scene01.visitedHotspots.includes(id)){
+
+        Scene01.visitedHotspots.push(id);
+
+    }
+
+}
+
+function canEnterShop(){
+
+    return Scene01.requiredHotspots.every(
+
+        id => Scene01.visitedHotspots.includes(id)
+
+    );
+
+}
