4a5dc264fb5794ba58d6a0c2c8b9295c00e7c195
Mike <mike.knoppix@gmail.com>
2026-08-08 22:30:00 +0200

    Create scene02.js

---
+++ chapters/chapter01/scene02.js
@@ -0,0 +1,22 @@
+// ======================================================
+// ESCENA 02
+// DENTRO DE LA TIENDA
+// ======================================================
+
+const Scene02 = {
+
+    id: "scene02",
+
+    name: "La Tienda del Viejo Bug",
+
+    image: "assets/images/escena02.png",
+
+    music: "assets/music/escena02.mp3",
+
+    ambient: null,
+
+    hotspots: [
+
+    ]
+
+};
